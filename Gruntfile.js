d7dfc21aecc6f48558e09fb9a7dc7be44fb60a88
bornkiller <hjj491229492@hotmail.com>
2015-03-02 00:55:08 +0800

    add jshint config file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,47 +4,47 @@ module.exports = function(grunt) {
   // 任务配置
   grunt.initConfig({
     karma: {
-        options: {
-            configFile: 'karma.conf.js'
+      options: {
+        configFile: 'karma.conf.js'
+      },
+
+      continuous: {
+        // pre-process matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            // source files, that you wanna generate coverage for
+            // do not include tests or libraries
+            // (these files will be instrumented by Istanbul)
+            'src/storage.js': ['coverage']
         },
 
-        continuous: {
-            // pre-process matching files before serving them to the browser
-            // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-            preprocessors: {
-                // source files, that you wanna generate coverage for
-                // do not include tests or libraries
-                // (these files will be instrumented by Istanbul)
-                'src/storage.js': ['coverage']
-            },
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        // plugin support: junit, should config junitReporter in use
+        // plugin support: coverage, should config coverageReporter and preprocessors in use
+        reporters: ['coverage'],
 
 
-            // test results reporter to use
-            // possible values: 'dots', 'progress'
-            // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-            // plugin support: junit, should config junitReporter in use
-            // plugin support: coverage, should config coverageReporter and preprocessors in use
-            reporters: ['coverage'],
+        // optionally, configure the reporter
+        coverageReporter: {
+            type : 'lcov',
+            dir : 'coverage/',
+            subdir: './'
+        },
 
 
-            // optionally, configure the reporter
-            coverageReporter: {
-                type : 'lcov',
-                dir : 'coverage/',
-                subdir: './'
-            },
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        // available options 'Chrome', 'IE', 'Firefox', 'PhantomJS' while 'IE8','IE9' approaching
+        browsers: ['PhantomJS'],
 
 
-            // start these browsers
-            // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-            // available options 'Chrome', 'IE', 'Firefox', 'PhantomJS' while 'IE8','IE9' approaching
-            browsers: ['PhantomJS'],
-
-
-            // Continuous Integration mode
-            // if true, Karma captures browsers, runs the tests and exits
-            singleRun: true
-        }
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true
+      }
     }
   });
 
